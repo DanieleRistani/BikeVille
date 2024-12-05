b12d8fc98b506215bd228fcbeb789590e29debe0
Daniele <ristanidaniele@yahoo.it>
2024-12-05 22:17:28 +0100

    add email

--- BikeVille/obj/Debug/net8.0/BikeVille.AssemblyInfo.cs
+++ BikeVille/obj/Debug/net8.0/BikeVille.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BikeVille")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d7cd9051116facfe06713adcd77693f36175f5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b61dd081618184300abadc9c436566e4b3b4ec43")]
 [assembly: System.Reflection.AssemblyProductAttribute("BikeVille")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BikeVille")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
